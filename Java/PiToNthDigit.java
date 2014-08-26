efe40ea0eb6922131d88211ebddbc56fb1139fff
dhd12391 <dhd12391@mail.sfsu.edu>
2014-08-26 11:31:03 -0700

    PiToNthDigit.java: Solved overflow problem (changed algorithm from using type conversion to Math.floor() )

--- Java/PiToNthDigit.java
+++ Java/PiToNthDigit.java
@@ -2,9 +2,11 @@
   Project: **Find PI to the Nth Digit**
   Author: David Hua (Github: dhd12391)
   Date: 8/25/14
-  Details: If program is run on command prompt, user can input an integer greater
-	       than zero as the first argument N in order for the program to run.
-		   Else, program will prompt user to do so during execution.
+  Description: Enter a number and have the program generate PI up to that many decimal places. 
+			   Keep a limit to how far the program will go.   
+  Program Details: If program is run on command prompt, user can input an integer greater
+				   than zero as the first argument N in order for the program to run.
+				   Else, program will prompt user to do so during execution.
 ******************************************************/
 import java.math.*;;
 import java.util.Scanner;
@@ -37,11 +39,10 @@ public class PiToNthDigit{
 		}
 		
 		//TODO: if N >= 10, piToNthDigit is overflowed due to exponent being very high
-		//RESOLVE: Instead of using pow(), is there a way to chop the tail at a certain position?
-		// 3.141592... - (3.141592... % pow(1,-(N-1))
-		// e.g: N = 3 -> 3.1415 - (3.1415 % pow (1,-3)) = 3.1415 - (3.1415 % 0.01) = 3.1415 - 0.0015 = 3.1400
+		//RESOLVE: pow() was not the problem -- int casting was due to its lack of memory compared to double
+		//			How to get result without type casting... Math.floor()!
 		if( N >= 0 ){ //if N == 0, return 0.0; 
-			piToNthDigit = (double) ((int) (PI * Math.pow(10, N-1) )) / Math.pow(10, N-1);
+			piToNthDigit = ( Math.floor(PI * Math.pow(10, N-1)) ) / Math.pow(10, N-1);
 			System.out.println( "Pi with " + N + " digits is: " + piToNthDigit);
 		}
 		//e.g. For N=3: 3.1415... * (10 ^ 2) = 314.15... ; int casting -> 314.15... becomes 314;
