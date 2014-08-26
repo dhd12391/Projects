0079de7d2e326dde77a7447cf97b62cd77b17917
dhd12391 <dhd12391@mail.sfsu.edu>
2014-08-26 01:38:05 -0700

    PiToNthDigit.java: implemented user input from console if argument wasn't provided. TODO: figure out how to deal with overflow of returned pi value (when N >= 10)

--- Java/PiToNthDigit.java
+++ Java/PiToNthDigit.java
@@ -6,32 +6,44 @@
 	       than zero as the first argument N in order for the program to run
 ******************************************************/
 import java.math.*;;
+import java.util.Scanner;
 
 public class PiToNthDigit{
 	public static void main(String[]args){
 		final double PI = Math.PI;
-		double piToNthDigit = 0.0;		
+		double piToNthDigit = 0.0;	
+		Scanner input = new Scanner(System.in);
+		int N;
 		
 		try{
-			final int N = Integer.parseInt(args[0]); //digit limit
-
-			if(N < 0){
-				System.out.println("The number given is a negative number");
-				return;
-			}
-			
-			if( N >= 0 ){ //if N == 0, return 0.0
-				piToNthDigit = (double) ((int) (PI * Math.pow(10, N-1) )) / Math.pow(10, N-1);
-				System.out.println( "Pi with " + N + " digits is: " + piToNthDigit);
-			}
-			//e.g. For N=3: 3.1415... * (10 ^ 2) = 314.15... ; int casting -> 314.15... becomes 314;
-			//double casting -> 314 becomes 314.0; 314.0 / 10^2 = 3.14			
-			
+			N = Integer.parseInt(args[0]); //digit limit	
 		}catch(NumberFormatException e){
 			System.out.println("The argument given is not an Integer.");
 			return;
 		}catch(ArrayIndexOutOfBoundsException e){
 			System.out.println("Please provide an Integer as an argument for the program.");
+			try{
+				N = input.nextInt();
+			}catch(NumberFormatException ex){
+				System.out.println("The argument given is not an Integer.");
+				return;
+			}
 		}
+		
+		if(N < 0){
+			System.out.println("The number given is a negative number");
+			return;
+		}
+		
+		//TODO: if N >= 10, piToNthDigit is overflowed due to exponent being very high
+		if( N >= 0 ){ //if N == 0, return 0.0; 
+			piToNthDigit = (double) ((int) (PI * Math.pow(10, N-1) )) / Math.pow(10, N-1);
+			System.out.println( "Pi with " + N + " digits is: " + piToNthDigit);
+		}
+		//e.g. For N=3: 3.1415... * (10 ^ 2) = 314.15... ; int casting -> 314.15... becomes 314;
+		//double casting -> 314 becomes 314.0; 314.0 / 10^2 = 3.14		
+		
+		//System.out.println("PI is " + PI); // DEBUG STATEMENT, CHECKING FOR PI
+	
 	}	
 }
