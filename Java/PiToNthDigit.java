566580610197e8f01ce8ba5f49d1e5db4ad297ba
dhd12391 <dhd12391@mail.sfsu.edu>
2014-08-25 19:33:16 -0700

    PiToNthDigit.java: added exception checking for no arguments and non-integer argument

--- Java/PiToNthDigit.java
+++ Java/PiToNthDigit.java
@@ -2,16 +2,29 @@ import java.math.*;;
 
 public class PiToNthDigit{
 	public static void main(String[]args){
-		int N = Integer.parseInt(args[0]); //digit limit
 		final double PI = Math.PI;
-		double piToNthDigit = 0.0;
+		double piToNthDigit = 0.0;		
+		
+		try{
+			final int N = Integer.parseInt(args[0]); //digit limit
 
-		if( N >= 0 ){ //if N is not 0 or greater, or is not an integer, return 0.0
-			piToNthDigit = (double) ((int) (PI * Math.pow(10, N-1) )) / Math.pow(10, N-1); 
+			if(N < 0){
+				System.out.println("The number given is a negative number");
+				return;
+			}
+			
+			if( N >= 0 ){ //if N == 0, return 0.0
+				piToNthDigit = (double) ((int) (PI * Math.pow(10, N-1) )) / Math.pow(10, N-1);
+				System.out.println( "Pi with " + N + " digits is: " + piToNthDigit);
+			}
+			//e.g. N=3 -> 3.1415 * (10 ^ 2) = 314.15 ; int casting -> 314.15 becomes 314;
+			//double casting -> 314 becomes 314.0; 314.0 / 10^2 = 3.14			
+			
+		}catch(NumberFormatException e){
+			System.out.println("The argument given is not an Integer.");
+			return;
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("Please provide an Integer as an argument for the program.");
 		}
-		//e.g. N=3 -> 3.1415 * (10 ^ 2) = 314.15 ; int casting -> 314.15 becomes 314;
-		//double casting -> 314 becomes 314.0; 314.0 / 10^2 = 3.14
-
-		System.out.println( "Pi with " + N + " digits is: " + piToNthDigit);
 	}	
 }
