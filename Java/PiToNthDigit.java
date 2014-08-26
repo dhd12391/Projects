82bd6d5f4f615d1bcc4e2b515ff96b4c9bab3732
dhd12391 <dhd12391@mail.sfsu.edu>
2014-08-26 10:37:50 -0700

    PiToNthDigit.java: Constructed (in comments) new algorithm to deal with overflow of pow()

--- Java/PiToNthDigit.java
+++ Java/PiToNthDigit.java
@@ -2,8 +2,9 @@
   Project: **Find PI to the Nth Digit**
   Author: David Hua (Github: dhd12391)
   Date: 8/25/14
-  Details: If program is run on command prompt, user must input an integer greater
-	       than zero as the first argument N in order for the program to run
+  Details: If program is run on command prompt, user can input an integer greater
+	       than zero as the first argument N in order for the program to run.
+		   Else, program will prompt user to do so during execution.
 ******************************************************/
 import java.math.*;;
 import java.util.Scanner;
@@ -36,6 +37,9 @@ public class PiToNthDigit{
 		}
 		
 		//TODO: if N >= 10, piToNthDigit is overflowed due to exponent being very high
+		//RESOLVE: Instead of using pow(), is there a way to chop the tail at a certain position?
+		// 3.141592... - (3.141592... % pow(1,-(N-1))
+		// e.g: N = 3 -> 3.1415 - (3.1415 % pow (1,-3)) = 3.1415 - (3.1415 % 0.01) = 3.1415 - 0.0015 = 3.1400
 		if( N >= 0 ){ //if N == 0, return 0.0; 
 			piToNthDigit = (double) ((int) (PI * Math.pow(10, N-1) )) / Math.pow(10, N-1);
 			System.out.println( "Pi with " + N + " digits is: " + piToNthDigit);
