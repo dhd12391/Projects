40e5b5e8330f689014c08454d97463815c59de57
dhd12391 <dhd12391@mail.sfsu.edu>
2014-08-26 12:01:52 -0700

    PiToNthDigit.java: Cleaning up

--- Java/PiToNthDigit.java
+++ Java/PiToNthDigit.java
@@ -4,15 +4,17 @@
   Date: 8/25/14
   Description: Enter a number and have the program generate PI up to that many decimal places. 
 			   Keep a limit to how far the program will go.   
-  Program Details: If program is run on command prompt, user can input an integer greater
-				   than zero as the first argument N in order for the program to run.
+  Program Details: If program is run on command prompt, user can input an integer >= 0 
+				   as the first argument N in order for the program to run.
 				   Else, program will prompt user to do so during execution.
+  Assumption: When N = 0, result is 3.0; When N = 1, result is 3.1; When N = 2, result is 3.14; etc.				   
 ******************************************************/
 import java.math.*;;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class PiToNthDigit{
-	public static void main(String[]args){
+	public static void main(String[]args) throws IOException{
 		final double PI = Math.PI;
 		double piToNthDigit = 0.0;	
 		Scanner input = new Scanner(System.in);
@@ -20,35 +22,33 @@ public class PiToNthDigit{
 		
 		try{
 			N = Integer.parseInt(args[0]); //digit limit	
+			
 		}catch(NumberFormatException e){
-			System.out.println("The argument given is not an Integer.");
+			System.out.println("ERROR: The argument given is not an Integer.");
 			return;
+			
 		}catch(ArrayIndexOutOfBoundsException e){
-			System.out.println("Please provide an Integer as an argument for the program.");
+			System.out.print("Please provide an Integer as an argument for the program: ");
 			try{
 				N = input.nextInt();
+				
 			}catch(NumberFormatException ex){
-				System.out.println("The argument given is not an Integer.");
+				System.out.println("ERROR: The argument given is not an Integer.");
 				return;
 			}
 		}
 		
 		if(N < 0){
-			System.out.println("The number given is a negative number");
+			System.out.println("ERROR: The number given is a negative number");
 			return;
 		}
-		
-		//TODO: if N >= 10, piToNthDigit is overflowed due to exponent being very high
-		//RESOLVE: pow() was not the problem -- int casting was due to its lack of memory compared to double
-		//			How to get result without type casting... Math.floor()!
-		if( N >= 0 ){ //if N == 0, return 0.0; 
-			piToNthDigit = ( Math.floor(PI * Math.pow(10, N-1)) ) / Math.pow(10, N-1);
-			System.out.println( "Pi with " + N + " digits is: " + piToNthDigit);
+
+		else{
+			piToNthDigit = ( Math.floor(PI * Math.pow(10, N)) ) / Math.pow(10, N);
+			System.out.println( "Pi with " + N + " decimal places is: " + piToNthDigit);
 		}
-		//e.g. For N=3: 3.1415... * (10 ^ 2) = 314.15... ; int casting -> 314.15... becomes 314;
-		//double casting -> 314 becomes 314.0; 314.0 / 10^2 = 3.14		
-		
-		//System.out.println("PI is " + PI); // DEBUG STATEMENT, CHECKING FOR PI
-	
+		//e.g. For N=3: 3.1415... * (10 ^ 3) = 3141.5... ;
+		//Math.floor() -> 3141.5 becomes 3141.0; 3141.0 / 10^3 = 3.141		
+
 	}	
 }
